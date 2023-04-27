e034fa4ba73a9a30253dd5d371efd1f4c37b5d86
Tran Xuan Loc <loc.tran@moreh.com.vn>
2023-04-27 12:07:12 +0000

    ignore

--- main.py
+++ main.py
@@ -96,15 +96,15 @@ class Layer(nn.Linear):
             self.opt.step()
         return self.forward(x_pos).detach(), self.forward(x_neg).detach()
 
-    
+
 def visualize_sample(data, name='', idx=0):
     reshaped = data[idx].cpu().reshape(28, 28)
     plt.figure(figsize = (4, 4))
     plt.title(name)
     plt.imshow(reshaped, cmap="gray")
     plt.show()
-    
-    
+
+
 if __name__ == "__main__":
     torch.manual_seed(1234)
     train_loader, test_loader = MNIST_loaders()
@@ -115,10 +115,10 @@ if __name__ == "__main__":
     x_pos = overlay_y_on_x(x, y)
     rnd = torch.randperm(x.size(0))
     x_neg = overlay_y_on_x(x, y[rnd])
-    
+
     for data, name in zip([x, x_pos, x_neg], ['orig', 'pos', 'neg']):
         visualize_sample(data, name)
-    
+
     net.train(x_pos, x_neg)
 
     print('train error:', 1.0 - net.predict(x).eq(y).float().mean().item())
